98d9aa179bc3e35c93aa8b3cc3da5ca2fb3be363
Lucas Gonçalves <103429796+luccaByte@users.noreply.github.com>
2024-09-02 20:07:44 +0000

    Vetores e Arrays: Adicionar elemento no final do vetor

---
+++ src/com/lucca/datastructure/tests/AulaTres.java
@@ -0,0 +1,13 @@
+package com.lucca.datastructure.tests;
+
+import com.lucca.datastructure.vetor.Vetor;
+
+public class AulaTres {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+    }
+}
--- src/com/lucca/datastructure/vetor/Vetor.java
+++ src/com/lucca/datastructure/vetor/Vetor.java
@@ -3,8 +3,45 @@ package com.lucca.datastructure.vetor;
 public class Vetor {
 
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    // opçao 1
+    // public void adiciona(String elemento) {
+    //     for(int i = 0; i < this.elementos.length; i++){
+    //         if (this.elementos[i] == null) {
+    //             this.elementos[i] = elemento;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // opção 2 - melhorando o método adiciona
+    // public void adiciona(String elemento) throws Exception {
+
+    //     if (this.tamanho < this.elementos.length) {
+            
+    //         this.elementos[this.tamanho] = elemento;
+    //         this.tamanho++;
+    //     } else {
+    //         throw new Exception("Vetor já está cheio, não é possível adicionar mais elementos.");
+    //     }
+    // }
+
+    //opçao 3
+    public boolean adiciona(String elemento) {
+
+        if (this.tamanho < this.elementos.length) {
+            
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
