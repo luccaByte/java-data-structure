2aaf7caba2dce9aa48e0b301a9eed0128fcdb4b6
Lucas Gonçalves <103429796+luccaByte@users.noreply.github.com>
2024-09-02 19:46:44 +0000

    initial commit

---
+++ src/com/lucca/datastructure/tests/AulaDois.java
@@ -0,0 +1,13 @@
+package com.lucca.datastructure.tests;
+
+import com.lucca.datastructure.vetor.Vetor;
+
+public class AulaDois {
+
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+        i++;
+    }
+}
---
+++ src/com/lucca/datastructure/vetor/Vetor.java
@@ -0,0 +1,10 @@
+package com.lucca.datastructure.vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+}
