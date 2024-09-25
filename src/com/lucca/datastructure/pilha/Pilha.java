44579cd330e92aa2b62403ac9379d211cac74e6d
lucas <lucaston2323@gmail.com>
2024-09-25 16:53:52 -0300

    introdução à estrutura de dados Pilha

---
+++ src/com/lucca/datastructure/pilha/Pilha.java
@@ -0,0 +1,14 @@
+package com.lucca.datastructure.pilha;
+
+import com.lucca.datastructure.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+
+    public Pilha() {
+        super();
+    }
+
+    public Pilha(int capacidade) {
+        super(capacidade);
+    }
+}
---
+++ src/com/lucca/datastructure/pilha/tests/Aula13.java
@@ -0,0 +1,13 @@
+package com.lucca.datastructure.pilha.tests;
+
+import com.lucca.datastructure.pilha.Pilha;
+
+public class Aula13 {
+    public static void main(String[] args) {
+
+        Pilha<Integer> pilha = new Pilha<Integer>();
+
+        System.out.println(pilha);
+        System.out.println(pilha.tamanho());
+    }
+}
