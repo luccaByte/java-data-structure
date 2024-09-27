5d488a18fdae2afc745bcd11559fbbcbf54358c3
lucas <lucaston2323@gmail.com>
2024-09-27 17:04:50 -0300

    empilhar elementos (push)

---
+++ src/com/lucca/datastructure/pilha/tests/Aula14.java
@@ -0,0 +1,16 @@
+package com.lucca.datastructure.pilha.tests;
+
+import com.lucca.datastructure.pilha.Pilha;
+
+public class Aula14 {
+    public static void main(String[] args) {
+        Pilha<Integer> pilha = new Pilha<Integer>();
+
+        for (int i = 1; i <= 11; i++) {
+            pilha.empilha(i);
+        }
+
+        System.out.println(pilha);
+        System.out.println(pilha.tamanho());
+    }
+}
